fb547210ef80ed244ffff0ac5605d5cab1c4a81a
D.Dvali <dvali.daviti@gmail.com>
2019-09-12 17:36:51 +0400

    Add DVL_Sync.Processor

---
+++ DVL_Sync.Processor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting.WindowsServices;
+using DVL_Sync.Abstractions;
+using DVL_Sync.Implementations;
+using AltaSoft.Extensions.RepetitiveTask;
+
+namespace DVL_Sync.Processor
+{
+    internal static class Program
+    {
+        internal static Task Main(string[] args)
+        {
+            var hostBuilder = Host.CreateDefaultBuilder()
+                                  .ConfigureServices(ConfigureServices);
+
+            return Environment.UserInteractive
+                   ? hostBuilder.RunConsoleAsync()
+                   : hostBuilder.UseServiceBaseLifetime().Build().RunAsync();
+        }
+
+        private static void ConfigureServices(HostBuilderContext arg1, IServiceCollection services)
+        {
+            services.AddSingleton<IFolderOperationEventsReader, FolderOperationEventsReaderFromJsonFile>()
+                    .AddSingleton<IFoldersSyncer, FoldersSyncer>();
+
+            //services.AddHostedRepetitiveTask("Execute",
+
+
+        }
+    }
+}
